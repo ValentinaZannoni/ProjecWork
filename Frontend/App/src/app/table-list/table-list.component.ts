ae4962a180548a62a12237b136cd9132fcdbe950
ValentinaZannoni <valentina.zannoni01@gmail.com>
2023-04-03 11:54:34 +0200

    aggiunta cartella backend

--- Frontend/App/src/app/table-list/table-list.component.ts
+++ Frontend/App/src/app/table-list/table-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import 'rxjs/add/operator/filter';
 
 @Component({
   selector: 'app-table-list',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableListComponent implements OnInit {
 
-  constructor() { }
+  orderby: string;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+   this.getProva()
+  }
+
+  getProva(){
+  //   this.route.queryParams
+  //   .subscribe(params => {
+  //     console.log(params); // { orderby: "price" }
+  //     this.orderby = params.orderby;
+  //     console.log(this.orderby); // price
+  //   }
+  // );
+  }
+
+  
+  getProvaWithFilters(){
+  //   this.route.queryParamMap
+  //   .subscribe((params) => {
+  //     this.paramsObject = { ...params.keys, ...params };
+  //     console.log(this.paramsObject);
+  //   }
+  // );
+  
   }
 
 }
