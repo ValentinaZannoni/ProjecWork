30bdf1dda6f03323ba2388e066c004bf15c8cb1d
lorena-jivan <lorena.jivan.studio@fitstic-edu.com>
2023-04-04 10:11:46 +0200

    modifica table-list

--- Frontend/App/src/app/table-list/table-list.component.ts
+++ Frontend/App/src/app/table-list/table-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import 'rxjs/add/operator/filter';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-table-list',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableListComponent implements OnInit {
 
-  constructor() { }
+  orderby: string;
+
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit() {
+   console.log("aaaaaaaaaaaaaaaaa")
+   this.getProva()
+  }
+
+  getProva(){
+    // questo è per i parametri 
+    // let headers = new HttpHeaders({
+    //   'x-rapidapi-host': 'random-facts2.p.rapidapi.com',
+    //   'x-rapidapi-key': 'your-api-key'
+    // });
+    this.http
+    // con parametri 
+
+      // .get<any>('https://random-facts2.p.rapidapi.com/getfact', {
+      //   headers: headers
+      // })
+
+     // senza parametri 
+
+       .get<any>('http://192.168.33.171:80/items')
+      .subscribe(data => {
+        console.log(data);
+      });
+      // console.log("data");
+
+      }
+
+  
+  getProvaWithFilters(){
+  //   this.route.queryParamMap
+  //   .subscribe((params) => {
+  //     this.paramsObject = { ...params.keys, ...params };
+  //     console.log(this.paramsObject);
+  //   }
+  // );
+  
   }
 
 }
