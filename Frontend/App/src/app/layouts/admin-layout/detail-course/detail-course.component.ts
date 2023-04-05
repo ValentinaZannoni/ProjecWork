d08544ee3e087c18f7d0820d26b3b634b32045d5
lorena-jivan <lorena.jivan.studio@fitstic-edu.com>
2023-04-05 17:56:48 +0200

    prova dettaglio corsi statica

--- Frontend/App/src/app/layouts/admin-layout/detail-course/detail-course.component.ts
+++ Frontend/App/src/app/layouts/admin-layout/detail-course/detail-course.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Course } from './models';
 
 @Component({
   selector: 'detail-course',
@@ -9,6 +10,16 @@ import { ActivatedRoute } from '@angular/router';
 export class DetailCourseComponent implements OnInit {
 
   courseid: string;
+
+  course: Course = {
+    id: 6487,
+    title : "Prova",
+    subject : "Programmazione",
+    price: 50.00,
+    description : "Questo è un bel corso",
+    teacher : null,
+    students: null,
+  }
 
   constructor(private route: ActivatedRoute) { }
 
