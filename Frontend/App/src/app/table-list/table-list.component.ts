4b02306c19bd6679d74022fba94b59e2f232f1f8
ValentinaZannoni <valentina.zannoni01@gmail.com>
2023-04-12 21:03:01 +0200

    Modifiche generali di layout

--- Frontend/App/src/app/table-list/table-list.component.ts
+++ Frontend/App/src/app/table-list/table-list.component.ts
@@ -13,59 +13,59 @@ import { AuthService } from '../layouts/admin-layout/auth.service';
 export class TableListComponent implements OnInit {
 
   orderby: string;
-  courses: Course[] = [{
-    id: 6487,
-    title : "Prova",
-    subject : "Programmazione",
-    price: null,
-    description : "Questo è un bel corso",
-    teacher : {
-      id: 72387,
-      name: "Valentina",
-      surname: "Zannoni",
-      phoneNumber: "3394873635",
-      age: "10",
-      role: "S",     // T = teacher , S = Student
-      cf: "ZNNVNT01T52E730L",
-      emailAddress: "subit@io.it",
-      password: null
-    },
-    students: null,
-  },
-  {
-    id: 5544,
-    title : "Prova2",
-    subject : "Matematica",
-    price: null,
-    description : "Questo è un brutto corso",
-    teacher : {
-      id:345,
-      name: "Maria",
-      surname: "Gianca",
-      phoneNumber: "6453738635",
-      age: "40",
-      role: null,   // T = teacher , S = Student
-      cf: "DRTVTH65T56N764G",
-      emailAddress: "emailprof@gmail.com",
-      password: null
-    },
-    students: null,
-  }
-];
-  // courses: Course[]
+//   courses: Course[] = [{
+//     id: 6487,
+//     title : "Prova",
+//     subject : "Programmazione",
+//     price: null,
+//     description : "Questo è un bel corso",
+//     teacher : {
+//       id: 72387,
+//       name: "Valentina",
+//       surname: "Zannoni",
+//       phoneNumber: "3394873635",
+//       age: "10",
+//       role: "S",     // T = teacher , S = Student
+//       cf: "ZNNVNT01T52E730L",
+//       emailAddress: "subit@io.it",
+//       password: null
+//     },
+//     students: null,
+//   },
+//   {
+//     id: 5544,
+//     title : "Prova2",
+//     subject : "Matematica",
+//     price: null,
+//     description : "Questo è un brutto corso",
+//     teacher : {
+//       id:345,
+//       name: "Maria",
+//       surname: "Gianca",
+//       phoneNumber: "6453738635",
+//       age: "40",
+//       role: null,   // T = teacher , S = Student
+//       cf: "DRTVTH65T56N764G",
+//       emailAddress: "emailprof@gmail.com",
+//       password: null
+//     },
+//     students: null,
+//   }
+// ];
+  courses: Course[]
 
   constructor(public auth: AuthService, private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit() {
-  //  this.getCourses()
-   if(this.auth.isLogged) console.log("sono loggatoooo")
+  this.getCourses()
+   if(this.auth.isLogged) console.log("sono loggato")
   }
 
   getCourses(){
-    this.http.get('http://192.168.33.171:80/courses').subscribe((data: any[]) => {
+    this.http.get('http://192.168.220.1:80/courses').subscribe((data: any[]) => {
       this.courses = data.map(user => Object.assign(new Course(), user));
 
-      console.log(this.courses);
+      console.log("aaaa", this.courses);
     });
   }
 }
