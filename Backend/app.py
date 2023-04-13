9c955a0a5114e6f5f2e1089fa7a9fbc0d946f866
LucaStudio22 <luca.bottoni.studio@gmail.com>
2023-04-13 17:39:25 +0200

    Course-Student create e delete, all students

--- Backend/app.py
+++ Backend/app.py
@@ -13,17 +13,6 @@ if __name__ == '__main__':
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 
-class Item(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  titolo = db.Column(db.String(80), unique=True, nullable=False)
-  regista = db.Column(db.String(80), unique=True, nullable=False)
-  compagnia = db.Column(db.String(80), unique=True, nullable=False)
-
-  def __init__(self, titolo, regista, compagnia):
-    self.titolo = titolo
-    self.regista = regista
-    self.compagnia = compagnia
-
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(80), nullable=False)
@@ -62,7 +51,7 @@ class Course(db.Model):
 
 class Students(db.Model):
   idCourse = db.Column(db.Integer, primary_key=True)
-  idStudents = db.Column(db.Integer)
+  idStudents = db.Column(db.ARRAY(db.Integer), default=[])
 
   def __init__(self, idCourse, idStudents):
     self.idCourse = idCourse
@@ -70,13 +59,7 @@ class Students(db.Model):
 
 db.create_all()
 
-# User
-@app.route('/users/<id>', methods=['GET'])
-def get_user(id):
-  user = User.query.get(id)
-  del user.__dict__['_sa_instance_state']
-  return jsonify(user.__dict__)
-
+# User get_all
 @app.route('/users', methods=['GET'])
 def get_users():
   users = []
@@ -85,6 +68,14 @@ def get_users():
     users.append(user.__dict__)
   return jsonify(users)
 
+# User get_single
+@app.route('/users/<id>', methods=['GET'])
+def get_user(id):
+  user = User.query.get(id)
+  del user.__dict__['_sa_instance_state']
+  return jsonify(user.__dict__)
+
+# User create
 @app.route('/users', methods=['POST'])
 def create_user():
   body = request.get_json()
@@ -92,20 +83,23 @@ def create_user():
   db.session.commit()
   return "user created"
 
-# Course
-@app.route('/courses', methods=['POST'])
-def create_courses():
+# User update
+@app.route('/users/<id>', methods=['PUT'])
+def update_user(id):
   body = request.get_json()
-  db.session.add(Course(title=body['title'], description=body['description'], subject=body['subject'], price=body['price'], teacherId=body['teacherId']))
+  db.session.query(User).filter_by(id=id).update(
+    dict(name=body['name'], surname=body['surname'], phoneNumber=body['phoneNumber'], emailAddress=body['emailAddress'], password=body['password'], age=body['age'], role=body['role'], cf=body['cf']))
   db.session.commit()
-  return "course created"
+  return "user updated"
 
-@app.route('/courses/<id>', methods=['GET'])
-def get_course(id):
-  course = Course.query.get(id)
-  del course.__dict__['_sa_instance_state']
-  return jsonify(course.__dict__)
+# User delete
+@app.route('/users/<id>', methods=['DELETE'])
+def delete_user(id):
+  db.session.query(User).filter_by(id=id).delete()
+  db.session.commit()
+  return "user deleted"
 
+# Course get_all
 @app.route('/courses', methods=['GET'])
 def get_courses():
   courses = []
@@ -114,38 +108,119 @@ def get_courses():
     courses.append(item.__dict__)
   return jsonify(courses)
 
-# item
-@app.route('/items/<id>', methods=['GET'])
-def get_item(id):
-  item = Item.query.get(id)
-  del item.__dict__['_sa_instance_state']
-  return jsonify(item.__dict__)
+# Course get_single
+@app.route('/courses/<id>', methods=['GET'])
+def get_course(id):
+  course = Course.query.get(id)
+  del course.__dict__['_sa_instance_state']
+  return jsonify(course.__dict__)
 
-@app.route('/items', methods=['GET'])
-def get_items():
-  items = []
-  for item in db.session.query(Item).all():
-    del item.__dict__['_sa_instance_state']
-    items.append(item.__dict__)
-  return jsonify(items)
+# Course get_all_byStudentId
+@app.route('/courses/student/<studentId>', methods=['GET'])
+def get_courses_byStudentId(studentId):
+  courses = []
+  for s in db.session.query(Students).all():
+    del s.__dict__['_sa_instance_state']
+    if int(studentId) in s.__dict__['idStudents']:
+      idC = s.__dict__['idCourse']
+      course = Course.query.get(idC)
+      del course.__dict__['_sa_instance_state']
+      courses.append(course.__dict__)
+  return jsonify(courses)
 
-@app.route('/items', methods=['POST'])
-def create_item():
+# Course get_all_byStudentId not student
+@app.route('/courses/not-student/<studentId>', methods=['GET'])
+def get_courses_not_byStudentId(studentId):
+  courses = []
+  for s in db.session.query(Students).all():
+    del s.__dict__['_sa_instance_state']
+    if int(studentId) not in s.__dict__['idStudents']:
+      idC = s.__dict__['idCourse']
+      course = Course.query.get(idC)
+      del course.__dict__['_sa_instance_state']
+      courses.append(course.__dict__)
+  return jsonify(courses)
+
+# Course create
+@app.route('/courses', methods=['POST'])
+def create_courses():
   body = request.get_json()
-  db.session.add(Item(body['titolo'], body['regista'], body['compagnia']))
+  db.session.add(Course(title=body['title'], description=body['description'], subject=body['subject'], price=body['price'], teacherId=body['teacherId']))
+  db.session.commit()
+  return "course created"
+
+# Course update
+@app.route('/courses/<id>', methods=['PUT'])
+def update_course(id):
+  body = request.get_json()
+  db.session.query(Course).filter_by(id=id).update(
+    dict(title=body['title'], description=body['description'], subject=body['subject'], price=body['price'], teacherId=body['teacherId']))
+  db.session.commit()
+  return "course updated"
+
+# Course delete
+@app.route('/courses/<id>', methods=['DELETE'])
+def delete_course(id):
+  db.session.query(Course).filter_by(id=id).delete()
+  db.session.commit()
+  return "course deleted"
+
+# Course and Student create
+@app.route('/courses-students', methods=['POST'])
+def create_coursesStudents():
+  body = request.get_json()
+  db.session.add(Course(title=body['title'], description=body['description'], subject=body['subject'], price=body['price'], teacherId=body['teacherId']))
+  db.session.commit()
+  c = db.session.query(Course).order_by(Course.id.desc()).first()
+  db.session.add(Students(idCourse = c.__dict__['id'], idStudents = []))
+  db.session.commit()
+  return "course and students created"
+
+# Course and Student delete
+@app.route('/courses-students/<id>', methods=['DELETE'])
+def delete_coursesStudents(id):
+  db.session.query(Course).filter_by(id=id).delete()
+  db.session.commit()
+  db.session.query(Students).filter_by(idCourse=id).delete()
+  db.session.commit()
+  return "course and students deleted"
+
+# student get_all
+@app.route('/students', methods=['GET'])
+def get_students():
+  students = []
+  for s in db.session.query(Students).all():
+    del s.__dict__['_sa_instance_state']
+    students.append(s.__dict__)
+  return jsonify(students)
+
+# student get_single
+@app.route('/students/<idCourse>', methods=['GET'])
+def get_student(idCourse):
+  student = Students.query.get(idCourse)
+  del student.__dict__['_sa_instance_state']
+  return jsonify(student.__dict__)
+
+# student create
+@app.route('/students', methods=['POST'])
+def create_student():
+  body = request.get_json()
+  db.session.add(Students(idCourse=body['idCourse'], idStudents=body['idStudents']))
   db.session.commit()
   return "item created"
 
-@app.route('/items/<id>', methods=['PUT'])
-def update_item(id):
+# student update
+@app.route('/students/<idCourse>', methods=['PUT'])
+def modify_student(idCourse):
   body = request.get_json()
-  db.session.query(Item).filter_by(id=id).update(
-    dict(titolo=body['titolo'], regista=body['regista'], compagnia=body['compagnia']))
+  db.session.query(Students).filter_by(idCourse=idCourse).update(
+    dict(idStudents = body['idStudents']))
   db.session.commit()
-  return "item updated"
+  return "student updated"
 
-@app.route('/items/<id>', methods=['DELETE'])
-def delete_item(id):
-  db.session.query(Item).filter_by(id=id).delete()
+# student delete
+@app.route('/students/<idCourse>', methods=['DELETE'])
+def delete_student(idCourse):
+  db.session.query(Students).filter_by(idCourse=idCourse).delete()
   db.session.commit()
-  return "item deleted"
+  return "student deleted"
